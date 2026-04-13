eab75df4587c69c3e8d4f22e67e28063f6271726
amrit <vishal179singh@gmail.com>
2026-04-13 23:21:25 +0530

    push

--- app/module/auth/auth.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import validate from "../../common/middleware/validate.middleware";
-import RegisterDto from "./dto/register.dto";
-import * as controller from "./auth.controller.js"
-
-const router = Router()
-router.post("/register",validate(RegisterDto),controller.register)
-router.get("/login",va)
--- app/module/auth/dto/login.dto.js
+++
@@ -1,9 +0,0 @@
-import BaseDto from "../../../common/dto/base.dto";
-import joi from "joi";
-class LoginDto extends BaseDto{
-    static schema = joi.object({
-        email:joi.string().email().lowercase().required(),
-        password:joi.string().required()
-    })
-}
-export default LoginDto
